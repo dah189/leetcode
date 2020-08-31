74dc8b758a3502797b1639607e5560f42ccbcafb
Danlan <dah189@pitt.edu>
2020-08-31 12:23:07 +0800

    Update 0557_Reverse_Words_in_a_String_III.java
    the new version, neat codes

--- 0557_Reverse_Words_in_a_String_III.java
+++ 0557_Reverse_Words_in_a_String_III.java
@@ -1,18 +1,10 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder word = new StringBuilder();
         StringBuilder ans = new StringBuilder();
-        s = s + " ";
-        for(int i = 0; i < s.length(); i++){
-            word.append(s.charAt(i));
-            if(s.charAt(i) == ' '){
-                word.reverse();
-                ans.append(word);
-                word.setLength(0);
-            }
-        }
-        
-        ans.deleteCharAt(0);
-        return ans.toString();
+        String[] words = s.split(" ");
+        for(String word: words){
+            ans.append(new StringBuilder(word).reverse().toString() +" ");
+        }        
+        return ans.toString().trim();
     }
 }
